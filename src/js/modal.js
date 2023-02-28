84f2e60892389e778842d651bb4879d5032b64b3
EvgenBatt <evgenmgid@gmail.com>
2023-03-01 00:37:18 +0200

    Update Modal js
    Зробив один файл js для модалок, та оновив селектори визову модалок на html сторінках. Видалив зайві js скріпти.

---
+++ src/js/modal.js
@@ -0,0 +1,46 @@
+(() => {
+  const modals = [
+    {
+      openModalBtns: document.querySelectorAll('[data-price-open]'),
+      closeModalBtns: document.querySelectorAll('[data-price-close]'),
+      modal: document.querySelector('[data-price]'),
+    },
+    {
+      openModalBtns: document.querySelectorAll('[data-room-open]'),
+      closeModalBtns: document.querySelectorAll('[data-room-close]'),
+      modal: document.querySelector('[data-room]'),
+    },
+    {
+      openModalBtns: document.querySelectorAll('[data-table-open]'),
+      closeModalBtns: document.querySelectorAll('[data-table-close]'),
+      modal: document.querySelector('[data-table]'),
+    },
+  ];
+
+  function initModal(modal) {
+    if (
+      !modal.openModalBtns.length ||
+      !modal.closeModalBtns.length ||
+      !modal.modal
+    ) {
+      // Если селекторы не найдены, выходим из функции
+      return;
+    }
+
+    modal.openModalBtns.forEach(btn => {
+      btn.addEventListener('click', toggleModal.bind(null, modal));
+    });
+
+    modal.closeModalBtns.forEach(btn => {
+      btn.addEventListener('click', toggleModal.bind(null, modal));
+    });
+  }
+
+  function toggleModal(modal) {
+    modal.modal.classList.toggle('is-hidden');
+  }
+
+  modals.forEach(modal => {
+    initModal(modal);
+  });
+})();
