0902e43664da2e6bdef60d787ea97e3b542ac593
EvgenBatt <evgenmgid@gmail.com>
2023-03-01 01:24:20 +0200

    Update menu-active.js
    Доробив js код, щоб не використовувати 2 js для активного меню на різних сторінках

--- src/js/menu-active.js
+++ src/js/menu-active.js
@@ -1,6 +1,13 @@
 var links = document.getElementsByClassName('nav-link');
 for (var i = 0; i < links.length; i++) {
   if (links[i].href == window.location.href) {
-    links[i].classList.add('current-other');
+    if (
+      window.location.pathname == '/' ||
+      window.location.pathname.endsWith('/index.html')
+    ) {
+      links[i].classList.add('current-index');
+    } else {
+      links[i].classList.add('current-other');
+    }
   }
 }
