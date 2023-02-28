970093705c93bb088804c1e38a3e2db4d0f28604
Aleksandr Avramov <ke.avramov@gmail.com>
2023-02-28 02:24:10 +0200

    footer-end

--- src/js/menu-active.js
+++
@@ -1,6 +0,0 @@
-var links = document.getElementsByTagName("a");
-for (var i = 0; i < links.length; i++) {
-    if (links[i].href == window.location.href) {
-        links[i].classList.add("current-other");
-    }
-}
