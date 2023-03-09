5a4c0873e8b31be203545a0e19734efe3ededc03
Aleksandr Avramov <ke.avramov@gmail.com>
2023-03-09 17:16:17 +0200

    Rew

---
+++ src/js/menu-slider.js
@@ -0,0 +1,35 @@
+$('.menu-page-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 1300,
+  variableWidth: true,
+  // centerPadding: '100px',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
